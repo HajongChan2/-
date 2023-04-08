49d82be2a83ba06eca1aede62dedc4b5570b4232
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-04-08 23:19:33 +0900

    vv

--- 마음이(유기견 사이트)/js/addr.js
+++ 마음이(유기견 사이트)/js/addr.js
@@ -1,25 +1,33 @@
-// $(document).ready(function(){
-    
-//     $("#btn").click(function(){
-//         let add_detail = $("#address_datail").val();
-//         let mem_add = $("#member_addr").val();
-//         let member_address = {
-//             address : mem_add,
-//             address_detail : add_detail
-//         }
-
-//         $.ajax({
-//             tyep : "POST",
-//             url : "/address",
-//             data : member_address,
-//             success : function(){
-//                 console.log("성공");
-//             }
-
-//         });
+$(document).ready(function(){
+    $("#btn").click(function(){
         
-//     });
-// });
+        console.log(selbox);
+    });
+    $("#btn").click(function(){
+        let add_detail = $("#address_datail").val();
+        let mem_add = $("#member_addr").val();
+        let phone = $("#phone").val();
+        let selbox = $("#selbox option:selected").text();
+
+        let member_address = {
+            address : mem_add,
+            address_detail : add_detail,
+            phone : phone,
+            selbox : selbox
+        }
+
+        $.ajax({
+            tyep : "POST",
+            url : "/address",
+            data : member_address,
+            success : function(){
+                console.log("성공");
+            }
+
+        });
+        
+    });
+});
 function data(){
     new daum.Postcode({
         oncomplete: function(data) {
