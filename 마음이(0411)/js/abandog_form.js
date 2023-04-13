d2d6d6f01ece79bd3ee193f39c77a692f10252ab
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-04-13 22:35:55 +0900

    asd

--- 마음이(0411)/js/abandog_form.js
+++ 마음이(0411)/js/abandog_form.js
@@ -4,4 +4,31 @@ function checkNumber(event) {
     }
     
     return false;
-  }
+}
+$(document).ready(function(){
+    
+
+    $("#submit").on('click', function(){
+        let title = $("#title").val();
+        let phone = $("#register").val();
+        let addr = $("#member_addr").val();
+        let saddr = $("#kind").val();
+        let memo = $("text_box").val();
+
+        const application = {
+            title : title,
+            phone : phone,
+            addr : addr,
+            saddr : saddr,
+            memo : memo
+        }
+        $.ajax({
+            type : "POST",
+            url : "/address",
+            data : application,
+            success : function(data){
+                alert("성공");
+            }
+        }); 
+    });
+});
