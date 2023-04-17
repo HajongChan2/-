0fb8fac48dd00d2c595d5925674d55eb315b6d02
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-04-17 20:44:30 +0900

    asd

--- 마음이(0411)/js/payment.js
+++ 마음이(0411)/js/payment.js
@@ -20,21 +20,20 @@ $(document).ready(function(){
     let direct_message;
     let choice;
     let CountData;
-    let count;
     if(localStorage.getItem('count')){
         CountData = localStorage.getItem('count');
         $("#count").val(CountData);
-        $("#counts").text(CountData);
+        console.log($("#count").val());
     }
-    
+    let count = $("#count").val();
+    $(".count").text(count);
     $("#btn").click(function(){
         let add_detail = $("#address_detail").val();
         let mem_add = $("#member_addr").val();
         let phone = $("input[name=phone]").val();
         let name = $("#user_name").val();
         let payment_method = $('input[name=payment_method]:checked').val();
-        count = parseInt($("#counts").text());
-        console.log(count);
+        count = $("#count").val();
         direct_message = $("#direct_input");
         choice = $("#selbox option:selected");
         if(choice.val() == 4){
