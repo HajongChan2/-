d70f3acd89a107602fa7b1430f36d4533ddd2aa3
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-04-27 14:59:50 +0900

    asd

--- kkkkkk/igonan/src/main/resources/static/js/silde.js
+++ kkkkkk/igonan/src/main/resources/static/js/silde.js
@@ -6,8 +6,26 @@ $(function() {
         infinite: true,
         autoplay: true,
         speed: 1900,
+        delay: 2500,
         slidesPerView: 1,
 		paginationClickable: true,
         loop: true
     });
 });
+
+var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
