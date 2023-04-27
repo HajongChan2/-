0fd63eab0f850bd1a754487b7c0023e63c835994
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-04-27 15:06:54 +0900

    Update silde.js

--- kkkkkk/igonan/src/main/resources/static/js/silde.js
+++ kkkkkk/igonan/src/main/resources/static/js/silde.js
@@ -12,20 +12,3 @@ $(function() {
         loop: true
     });
 });
-
-var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
