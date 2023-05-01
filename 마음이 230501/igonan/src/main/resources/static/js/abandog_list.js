97665e487d8a566c4e660dd69e3c9b55cee6b2b7
banyaman <128017957+banyaman@users.noreply.github.com>
2023-05-01 17:34:35 +0900

    Add files via upload

--- 마음이 230501/igonan/src/main/resources/static/js/abandog_list.js
+++ 마음이 230501/igonan/src/main/resources/static/js/abandog_list.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
+
+    $.ajax({
+        type : "POST",
+        url : "/abandog/list",
+        dataType : "json",
+        success : function(data){
+            console.log(data);
+        }
+    });
+
+
     $(".abandoned_dog-text p").on('click',function(e){
         let na = $(e.target);
         let a = na.text();
         console.log(a);
     });
-    $.ajax({
-        type : "POST",
-        dataType : "JSON",
-        url : "abandog/list"
-        success : function(data){
-            console.log(data); 
-        }
-    });
+
 });
