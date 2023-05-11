05526e68954236fc36376806e5d3eba200ba2e12
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-05-11 21:21:50 +0900

    asd

--- 마음이 230507/igonan/src/main/java/com/example/igonan/Controller/MindController.java
+++ 마음이 230507/igonan/src/main/java/com/example/igonan/Controller/MindController.java
@@ -1,17 +1,8 @@
 package com.example.igonan.Controller;
 
 
-import com.example.igonan.Service.PaymentService;
-import com.example.igonan.dto.PaymentDTO;
-import com.example.igonan.mindmapper.Paymentmapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MindController {
