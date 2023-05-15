0a66c5d7a449b429a0860b95a96355d909b6e0cf
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-05-15 12:14:16 +0900

    asd

--- 마음이 230507/igonan/src/main/resources/static/js/main.js
+++ 마음이 230507/igonan/src/main/resources/static/js/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-    $( document ).ready( function() {
-        $( window ).scroll( function() {
-            if ( $( this ).scrollTop() > 200 ) {
-                $( '.top' ).fadeIn();
-            } else {
-                $( '.top' ).fadeOut();
-            }
-        } );
-        $( '.top' ).click( function() {
-            $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
-            return false;
-        } );
-    } );
-
     $.ajax({
         type : "POST",
         url : "/main/abandoglist",
