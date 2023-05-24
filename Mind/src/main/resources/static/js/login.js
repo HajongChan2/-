d0ed50bc00617f5c35ef689c4a542aecc54b252d
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-05-24 18:19:48 +0900

    Update login.js

--- Mind/src/main/resources/static/js/login.js
+++ Mind/src/main/resources/static/js/login.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         let id = $("#id").val();
         let pw = $("#pw").val();
         let a = document.referrer;
-        let addr = a.split('8080/', 2);
-
+        let b = a.split('8080/', 2);
+        let addr = `/${b}`;
 
         const login = {
             id : id,
@@ -22,5 +22,9 @@ $(document).ready(function(){
 
         });
     });
+    let a = document.referrer;
+    let b = a.split('8080/', 2);
+    let addr = `/${b[1]}`;
+    console.log(addr);
 
 });
