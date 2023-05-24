ec1cc2b7944fd04e17b7ecdfd55597f61254564e
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-05-24 16:46:24 +0900

    Update login.js

--- Mind/src/main/resources/static/js/login.js
+++ Mind/src/main/resources/static/js/login.js
@@ -2,19 +2,22 @@ $(document).ready(function(){
     $(".login_bt").click(function(){
         let id = $("#id").val();
         let pw = $("#pw").val();
+        let addr = document.referrer;
         const login = {
             id : id,
-            pw : pw
+            pw : pw,
+            addr : addr
         };
         $.ajax({
             type : "POST",
             url : "/logincheck",
             data : login,
             success : function(data){
-                sessionStorage.setItem("id", id);
                 console.log("성공");
             }
         });
     });
+
+
 
 });
