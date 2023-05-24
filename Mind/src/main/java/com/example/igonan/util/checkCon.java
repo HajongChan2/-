442f6d52485e949e28095fec1717b1f5c7f03b42
banyaman <128017957+banyaman@users.noreply.github.com>
2023-05-24 18:10:08 +0900

    ass
    ass ass in

--- Mind/src/main/java/com/example/igonan/util/checkCon.java
+++ Mind/src/main/java/com/example/igonan/util/checkCon.java
@@ -7,6 +7,7 @@ import com.example.igonan.mindmapper.Usermapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -52,29 +53,26 @@ public class checkCon {
         return "redirect:/login";
     }
 */
-@RequestMapping(value = "/logincheck")
+@RequestMapping(value = "/logincheck",method = RequestMethod.POST)
 @ResponseBody
 public String logincheck(HttpServletRequest rq, HttpSession hs) {
     String inputid = rq.getParameter("id");
      String inputpw = rq.getParameter("pw");
-    // String inputaddr = rq.getParameter("addr");
-    String inputaddr = "http://localhost:8080/info";
-     String useaddr = inputaddr.substring(21);
-     System.out.println(useaddr);
+
     List<UserDTO> loggedin = userService.getOneUser(inputid);
     System.out.println(loggedin.isEmpty());
     if(loggedin.isEmpty()==false){
         loggedInSession result = (userService.loginUser(inputid));
         if(inputpw.equals(result.getPw())){
             hs.setAttribute("username",result.getName());
-            hs.setAttribute("userid",result.getName());
+            hs.setAttribute("userid",inputid);
             String ssname = hs.getAttribute("username").toString();
             String ssid = hs.getAttribute("userid").toString();
             System.out.println("세션 생성 완료 이름 : "+ssname);
             System.out.println("세션 생성 완료 id : "+ssid);
           //  hs.removeAttribute("username");
 
-            return null;
+            return Script.tohref("/main");
         }else {
             System.out.println("비밀번호 틀림");
 
--- Mind/src/main/resources/static/js/login.js
+++ Mind/src/main/resources/static/js/login.js
@@ -2,25 +2,25 @@ $(document).ready(function(){
     $(".login_bt").click(function(){
         let id = $("#id").val();
         let pw = $("#pw").val();
-        let a = document.referrer;
-        let addr = a.split('8080/', 2);
+
+
         const login = {
             id : id,
             pw : pw,
-            addr : addr
+
         };
         $.ajax({
             type : "POST",
             url : "/logincheck",
             data : login,
             success : function(data){
-                
-            }
-        });
+                console.log(data);
+                alert('성공');
 
+            }
+
+        });
+        console.log(login +" dddddd");
     });
 
-    let addr = document.referrer;
-    let a = addr.split('8080/', 2);
-    console.log("/"+a[1]);
 });
