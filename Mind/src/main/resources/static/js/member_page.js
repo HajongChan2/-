ade4a43e866c9e31bfb83ea1d906dad4cf592e5a
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-05-29 18:38:13 +0900

    asd

--- Mind/src/main/resources/static/js/member_page.js
+++ Mind/src/main/resources/static/js/member_page.js
@@ -16,4 +16,34 @@ $(document).ready(function(){
            $(".member_info2").append(str);
        }
     });
+
+    $.ajax({
+        type : "POST",
+        url : "mypagebuytotal",
+        dataType : "json",
+        success : function (data){
+            str = '';
+            console.log(data);
+            str += `
+            <div class="border_box">
+                <h2>총 구매 횟수</h2>
+                <h2>${data[0].uCount}</h2>
+            </div>
+            <div class="border_box">
+                <h2>총 구매 금액</h2>
+                <h2>${data[0].uTotalpay}</h2>
+            </div>
+            <div class="border_box">
+                <h2>이번 달 구매 금액</h2>
+                <h2>0</h2>
+            </div>
+            <div class="border_box">
+                <h2>이번 달 쓴 총 금액</h2>
+                <h2>0</h2>
+            </div>
+            `
+            $("#purchase_dt").append(str);
+        }
+
+    });
 });
