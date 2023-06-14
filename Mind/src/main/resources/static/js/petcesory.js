b64340fd38f39358e0f020d6c3e70826d4a5a545
HajongChan2 <115999311+HajongChan2@users.noreply.github.com>
2023-06-14 21:20:22 +0900

    asd

--- Mind/src/main/resources/static/js/petcesory.js
+++ Mind/src/main/resources/static/js/petcesory.js
@@ -1,11 +1,13 @@
 let str = '';
+
 $(document).ready(function(){
+    let type = 'tag';
+    types_query();
     let addr = "product/detail";
-    let a = true;
-    if(a){
+    function types_query(){
         $.ajax({
             type: "POST",
-            url: "/petcesary/tag",
+            url: "/petcesary/"+type,
             dataType: "json",
             success: function (data) {
                 console.log(data);
@@ -14,6 +16,7 @@ $(document).ready(function(){
         });
         function productTag(product){
             str = '';
+            $('#container2').empty();
             product.map(function(tag){
 
                 str += `
@@ -31,107 +34,27 @@ $(document).ready(function(){
             });
             $("#container2").append(str);
         }
-
-        a = false;
     }
+
+
+
+    $(document).on('click','.pet_items',function(){
+        type = $(this).attr("id");
+        types_query();
+        console.log(type);
+    });
     $("#tag").on("click", function(){
         $(".line").animate({left:0},"fast");
-        $("#container2").empty();
-        $.ajax({
-            type: "POST",
-            url: "/petcesary/tag",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                productTag(data);
-            }
-        });
-        function productTag(product){
-            str = '';
-            product.map(function(tag){
 
-                str += `
-            <div class="petcesory-wrap">
-            <a href="${addr}">
-                        <div class="petcesory-image">
-                            <img src="${tag.prGallery}" alt="${tag.prName}">
-                            <p>${tag.prName}</p>
-                            <h2 id="number">${tag.prNum}</h2>
-                        </div>
-                    </a>                    
-                </div>`
-
-
-            });
-            $("#container2").append(str);
-        }
     });
 
     $("#chip").on("click", function(){
         $(".line").animate({left:'480px'},"fast");
-        $("#container2").empty();
-        $.ajax({
-            type: "POST",
-            url: "/petcesary/chip",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                productChip(data);
-            }
-        });
-        function productChip(product){
-            str = '';
-            product.map(function(chip){
 
-
-                str += `
-                 <div class="petcesory-wrap">
-                    <a href="${addr}">
-                        <div class="petcesory-image">
-                            <img src="${chip.prGallery}" alt="${chip.prName}">
-                            <p>${chip.prName}</p>
-                            <h2 id="number">${chip.prNum}</h2>
-                        </div>
-                    </a>                    
-                </div>`
-
-
-            });
-            $("#container2").append(str);
-        }
     });
     $("#etc").on("click", function(){
         $(".line").animate({left:'960px'},"fast");
-        $("#container2").empty();
-        $.ajax({
-            type: "POST",
-            url: "/petcesary/etc",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                productEtc(data);
-            }
-        });
-        function productEtc(product){
-            str = '';
-            product.map(function(etc){
 
-
-                str += `
-                 <div class="petcesory-wrap">
-                    <a href="${addr}">
-                        <div class="petcesory-image">
-                            <img src="${etc.prGallery}" alt="${etc.prName}">
-                            <p>${etc.prName}</p>
-                            <h2 id="number">${etc.prNum}</h2>
-                        </div>
-                    </a>                    
-                </div>`
-
-
-            });
-            $("#container2").append(str);
-        }
     });
 
     $(document).on('click','.petcesory-image',function(){
