f220dfcace8492ac41d1de4b78ef62a63f44aa3b
ahgh3412 <128660732+ahgh3412@users.noreply.github.com>
2023-06-15 00:19:08 +0900

    a

--- Mind/src/main/resources/static/js/mypage_abandoglist.js
+++ Mind/src/main/resources/static/js/mypage_abandoglist.js
@@ -5,31 +5,33 @@ $(document).ready(function () {
         dataType: "json",
         success: function (data) {
             abanapplist(data)
-
             console.log(data);
         }
     });
-
     function abanapplist(list) {
         let name = '';
         let str = '';
         list.map(function (mydog) {
             str +=
-                `<ul>
-                            <li>${mydog.id}</li>
-                            <li>${mydog.name}</li>
-                            <li>${mydog.dognum}</li>
-                            <li>${mydog.dogname}</li>
-                            <li>${mydog.phone}</li>
-                            <li>${mydog.addr}</li>
-                            <li>${mydog.saddr}</li>
-                            <li>${mydog.memo}</li>
-                            <li>${mydog.date}</li>
-                            <li>입양이 완료되었습니다.</li>
-                            </ul>
+                `<div>
+         <ul id="member_infor">
+            <li>${mydog.id}</li>
+            <li>${mydog.name}</li>
+            <li>${mydog.phone}</li>
+            <li>${mydog.addr}</li>
+            <li>${mydog.saddr}</li>
+            <li>${mydog.memo}</li>
+        </ul>
+        <ul id="dog_infor">
+            <li>${mydog.dognum}</li>
+            <li>${mydog.dogname}</li>
+        </ul>
+        <ul id="date_infor">
+            <li>${mydog.date}</li>
+            </ul>
+            </div>
 `
-
         });
-        $("#mydog_content").append(str);
+        $("#infor_container").append(str);
     }
 })
